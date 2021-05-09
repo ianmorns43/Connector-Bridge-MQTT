cf65ab2b65902bb70f7a52615781eec9429d4b47
Ian Morns <ian.morns@gmail.com>
2021-05-09 20:13:01 +0100

    first attempt at bidirectional hubitat blind

---
+++ apps/connector-hub.groovy
@@ -0,0 +1,154 @@
+/**
+ *  Connector Hub (Hubitat)
+ *
+ *  Copyright 2021 Ian Morns
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+definition(
+    name: "Connector Hub",
+    namespace: "ianmorns_rfremote",    
+    singleInstance: true,
+    author: "Ian Morns",
+    description: "Proxy to Dooya Connector Hub",
+    category: "Convenience",
+    iconUrl: "",
+    iconX2Url: "",
+    iconX3Url: "")
+
+
+preferences
+{  
+    page(name: "mainPage")
+}
+
+def mainPage()
+{
+    def validationResult = validateBroker()
+    
+	def page = dynamicPage(name: "mainPage",  title:"<b>Connector Hub Setup</b>\n", uninstall: true, install: true)
+    {
+        section("Hub Setup")
+        {
+            input "mqttBroker", "capability.healthCheck", title: "MQTT Broker Device", multiple: false, required: true, submitOnChange: true
+            if(!settings.brokerValidationResult)
+            {
+                paragraph "To be valid, the selected Broker must be a device of type 'MQTT Broker'."
+            }
+            input "brokerValidationResult", "text", title: "", multiple: false, required: true, submitOnChange: true, description: validationResult
+            input "hubTopic", "text", title: "Hub MQTT Topic", multiple: false, required: true, defaultValue: "dooya_connector_hub"
+            input "hubKey", "password", title: "Hub Key", multiple: false, required: true, description: "to get your key, open 'Connector' app on your mobile device. Go to 'About' and tap the connector icon 5 times."
+        }
+
+        section("<b>Window Shade Remotes</b>")
+        {
+            app(name: "windowShadeRemote", appName: "Dooya Window Shade", namespace: "ianmorns_rfremote", title: "Add a new window shade remote", multiple: true)
+        }
+
+        section()
+        {
+            input "enableLogging", "bool", title: "Enable debug logging for 30 minutes", multiple: false, defaultValue: false
+        }
+    }
+    
+    return page;
+}
+
+def validateBroker()
+{
+    def attributes = settings.mqttBroker?.getSupportedAttributes()
+    def broker = settings.mqttBroker;
+    def valid = broker?.hasAttribute('brokerDetails') && broker?.hasAttribute('brokerStatus') && broker?.hasCommand("getBrokerDetails")
+    logTrace("Is a valid Broker device? " + valid)
+
+    if(valid)
+    {
+        app?.updateSetting("brokerValidationResult", "The selected type is a valid Broker.")
+        return "The selected type is a valid Broker."
+    }
+    else
+    {
+        app?.removeSetting("brokerValidationResult")
+        return !settings.mqttBroker ? "No Broker device is selected." : "The selected device is of the wrong type."
+    }
+}
+
+def installed() 
+{
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() 
+{
+	log.debug "Updated with settings: ${settings}"
+
+    unschedule();
+	unsubscribe()
+	initialize()
+}
+
+def uninstalled() 
+{
+    log.debug "Uninstalled: ${app?.label}"
+}
+
+def initialize() 
+{       
+    if(!!enableLogging)
+    {
+        runIn(1800, "disableLogging");
+    }
+
+    subscribe(settings.mqttBroker, "brokerDetails", updateConnections)
+    subscribe(settings.mqttBroker, "brokerStatus", brokerStatusChanged)
+
+    if(settings.hubTopic != app.getState().previoushubTopic)
+    {
+        app.getState().previoushubTopic = settings.hubTopic;
+        updateConnections(null)
+    }
+}
+
+public updateConnections(evt)
+{
+    logTrace("updateConnections: " + evt)
+    app?.getChildApps().each{ it.updateConnections() }
+}
+
+public brokerStatusChanged(evt)
+{
+    app?.getChildApps().each{ it.brokerStatusChanged(evt) }
+}
+
+public getHubDetails()
+{
+    def details = [:]
+    settings.mqttBroker.getBrokerDetails(details)
+    details['hubTopic'] = settings.hubTopic
+    details['hubKey'] = settings.hubKey
+    return details
+}
+
+private disableLogging()
+{
+    app?.updateSetting("enableLogging", false);
+}
+
+void logTrace(msg)
+{
+    if(settings.enableLogging)
+    {
+        log.trace msg;
+    }
+}
---
+++ drivers/dooya-bidirectional-blind.groovy
@@ -0,0 +1,232 @@
+ import groovy.json.JsonSlurper
+ import groovy.json.JsonOutput
+
+ /**
+ *  Dooya Bidirectional Blind
+ *
+ *  Copyright 2020 Ian Morns
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Dooya Bidirectional Blind", namespace: "ianmorns_rfremote", author: "Ian Morns") {
+		capability "WindowShade"
+        capability "Configuration"
+        capability "Actuator"
+
+        attribute "hubStatus", "ENUM", ["online","offline","unknown"]
+	}
+   
+    section("Logging") 
+    {
+        input "enableLogging", "bool", title: "Enable debug logging for 30 minutes", multiple: false, defaultValue: true
+    }
+}
+
+// parse events into attributes
+def parse(String description)
+{
+    //TODO set hubStatus based in lwt
+    def message = interfaces.mqtt.parseMessage(description)
+    logTrace(message)
+
+    if(message.topic.endsWith("/lwt"))
+    {
+        sendEvent(name: "hubStatus", value: message.payload)
+    }
+}
+
+def setPosition(position)
+{
+    logTrace("SetPosition: ${position}");
+    
+    publish("moveShade", true, [position:position])
+}
+
+def open()
+{
+    logTrace("Open")
+    publish("openShade", true)
+}
+
+def close()
+{
+    logTrace("Close")
+    publish("closeShade", true)
+}
+
+def startPositionChange(direction)
+{
+    "${direction}"()
+}
+
+def stopPositionChange()
+{
+    publish("stopShade", true)
+}
+
+def publish(String command, Boolean includeKey)
+{
+    publish(command, includeKey, null)
+}
+
+/*
+dooya_connector_hub/command                     {command:"updateDeviceList"}
+                                                {command:"moveShade", mac:"<deviceMac>", key:"<hub-secret-key>", position:<newPosition>}
+                                                {command:"openShade", mac:"<deviceMac>", key:"<hub-secret-key>"}
+                                                {command:"closeShade", mac:"<deviceMac>", key:"<hub-secret-key>"}
+                                                {command:"stopShade", mac:"<deviceMac>", key:"<hub-secret-key>"}
+                                                {command:"updateDevce", mac:"<deviceMac>"}
+                                                */
+
+def publish(String command, Boolean includeKey, data)
+{
+    def details = parent.getConnectionDetails()
+    def payload = [command:command, mac:details.mac]
+
+    if(includeKey)
+    {
+        payload["key"] = details.hubKey
+    }
+
+    if(data)
+    {
+        payload << data
+    }
+
+    def topic = "${details.hubTopic}/command"
+    logTrace("Publish: ${topic}, ${payload}")
+    def payloadJson = JsonOutput.toJson(payload)
+
+    
+    logTrace("Publish: ${topic}, ${payloadJson}")
+    interfaces.mqtt.publish(topic, payloadJson)    
+}
+
+def installed()
+{
+    initialize();
+}
+
+def uninstalled()
+{
+    logTrace("Uninstalling")
+    if(interfaces.mqtt.isConnected())
+    {
+        interfaces.mqtt.disconnect()
+    }
+}
+
+def updated()
+{
+    unschedule()
+    initialize();
+}
+
+def initialize()
+{
+    if(settings.enableLogging)
+    {
+        runIn(1800, "disableLogging");
+    }
+}
+
+def subscritionTopics()
+{
+    def details = parent.getConnectionDetails()
+    def hubTopic = details.hubTopic
+    def hubKey = details.key
+
+    topics = []
+    topics << "${details.hubTopic}/status/${details.mac}"
+    topics << "${details.hubTopic}/lwt"
+    return topics
+}
+
+public configure()
+{
+    logTrace("Broker details updated")
+    if(interfaces.mqtt.isConnected())
+    {
+        subscritionTopics().each{ topic -> interfaces.mqtt.unsubscribe(topic) }
+        interfaces.mqtt.disconnect()
+    }
+    reconectIfNessecary()
+}
+
+public brokerStatusChanged(evt)
+{
+    if(evt.value == "offline")
+    {
+        sendEvent(name: "hubStatus", value: "unknown")
+    }
+    else
+    {
+        reconectIfNessecary()
+    }
+}
+
+def reconectIfNessecary()
+{
+    if(!interfaces.mqtt.isConnected())
+    {
+        logTrace("attempting to reconnect")
+        connectAndSubscribe()
+    }
+}
+
+def connectAndSubscribe()
+{
+    def connectionDetails = parent.getConnectionDetails()
+    if(!interfaces.mqtt.isConnected())
+    {
+        try
+        {
+            interfaces.mqtt.connect(connectionDetails.path, device.deviceNetworkId, connectionDetails.username, connectionDetails.password)
+        }
+        catch(exception)
+        {
+            logTrace("Connection error: " + exception)
+            sendEvent(name: "hubStatus", value: "unknown")
+        }
+    }    
+    
+    subscritionTopics().each
+    { 
+        topic -> interfaces.mqtt.subscribe(topic)
+        logTrace("Subscribed to: ${topic}")
+    }
+}
+
+def mqttClientStatus(String message)
+{
+    logTrace("mqttClientStatus $message")
+    
+    if(message.indexOf("Connection lost") >= 0 || message.indexOf("Client is not connected") >= 0)
+    {
+        logTrace("Lost connection so hubStatus is nolonger known")
+        sendEvent(name: "hubStatus", value: "unknown")
+    }
+}
+
+private disableLogging()
+{
+    device.updateSetting("enableLogging", false);
+}
+
+private logTrace(msg)
+{
+    if(settings.enableLogging)
+    {
+        log.trace msg
+    }
+}
+
